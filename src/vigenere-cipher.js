26331b2acdb075afe9fcf6b357a78624eb6729ab
Mikhail Bely <neverdream40@gmail.com>
2022-05-03 00:22:02 +0300

    fix: add solution for vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,112 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+    key = key.toUpperCase();
+    while (message.length > key.length) {
+      key += key.repeat(1);
+    }
+    key = key.slice(0, message.length);
+
+    let whitespaceIndex = [];
+    for (let i = 0; i < message.length; i++) {
+      if (message[i] === " ") whitespaceIndex.push([i]);
+    }
+    message = message.toUpperCase().split(" ").join("");
+
+    let siphere = [];
+    let startPosition = "A".charCodeAt(0);
+
+    for (let i = 0; i < key.length; i++) {
+      if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+        let keyPosition = key.charCodeAt(i) - startPosition;
+        let messagePosition = message.charCodeAt(i) - startPosition;
+        siphere.push(
+          String.fromCharCode(
+            startPosition + ((keyPosition + messagePosition) % 26)
+          )
+        );
+      } else {
+        siphere.push(message[i]);
+      }
+    }
+    whitespaceIndex.forEach((item) => {
+      siphere.splice(item, 0, " ");
+    });
+
+    return this.isDirect === true
+      ? siphere.join("")
+      : siphere.reverse().join("");
+  }
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+    key = key.toUpperCase();
+    while (message.length > key.length) {
+      key += key.repeat(1);
+    }
+    key = key.slice(0, message.length);
+
+    let whitespaceIndex = [];
+    for (let i = 0; i < message.length; i++) {
+      if (message[i] === " ") whitespaceIndex.push([i]);
+    }
+    message = message.toUpperCase().split(" ").join("");
+
+    let siphere = [];
+    let startPosition = "A".charCodeAt(0);
+
+    for (let i = 0; i < key.length; i++) {
+      if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+        let keyPosition = key.charCodeAt(i) - startPosition;
+        let messagePosition = message.charCodeAt(i) - startPosition;
+
+        siphere.push(
+          String.fromCharCode(
+            startPosition + ((messagePosition - keyPosition + 26) % 26)
+          )
+        );
+      } else {
+        siphere.push(message[i]);
+      }
+    }
+    whitespaceIndex.forEach((item) => {
+      siphere.splice(item, 0, " ");
+    });
+
+    return this.isDirect === true
+      ? siphere.join("")
+      : siphere.reverse().join("");
   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
